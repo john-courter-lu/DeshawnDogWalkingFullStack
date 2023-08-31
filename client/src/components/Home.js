3cc6ded326f82f235c9cd2c0be5b42b0b4cd37d1
John Lu <johnlu2006@gmail.com>
2023-08-30 21:51:33 -0500

    install MUI in the client folder where React is; import Google Web Fonts for Roboto and Icons Fonts; try Container, Grid, Item, and Paper

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,6 +2,20 @@ import { Link } from "react-router-dom";
 import { getDogs, getGreeting } from "../apiManager";
 import { useEffect, useState } from "react";
 
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import { experimentalStyled as styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
 export default function Home() {
   const [greeting, setGreeting] = useState({
     message: "Not Connected to the API",
@@ -28,17 +42,24 @@ export default function Home() {
   return (<>
     <p>{greeting.message}</p>
     <h2>List of Dogs</h2>
-    <article className="dogs">
-      {
-        dogs.map(dog => {
-          return (
-            <section className="dog" key={`dog--${dog.id}`}>
-              <div> <Link to={`/dogs/${dog.id}`}>{dog.name}</Link></div>
-            </section>
-          )
-        })
-      }
+    <Container className="dogs">
+      <Grid container spacing={{ xs: 2, md: 3 }} >
+        {
+          dogs.map(dog => {
+            return (
 
-    </article>
+              <Grid item xs={4} sm={3} md={2} className="dog" key={`dog--${dog.id} `}>
+                <Item>
+                  <Paper elevation={4}>
+                    <Link to={`/dogs/${dog.id}`}>{dog.name}</Link>
+                  </Paper>
+                </Item>
+              </Grid >
+
+            )
+          })
+        }
+      </Grid>
+    </Container>
   </>)
 }
